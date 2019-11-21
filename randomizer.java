01e600c0d3e77ee63e7ed02c82cd114dbaba4f08
Andrew Orlowski <57453461+uniqueuser-repo@users.noreply.github.com>
2019-11-20 19:34:39 -0500

    Remove unnecessary imports in randomizer.java

--- randomizer.java
+++ randomizer.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.stream.IntStream;
-
 public class randomizer {
     public static void main(String[] args) {
         int numThreads = 7;
